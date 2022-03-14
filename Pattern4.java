f1ea817016abba321a283b9113b6fdbd4a19c98c
ankit771 <101547149+ankit771@users.noreply.github.com>
2022-03-14 12:31:40 +0530

    Add files via upload

---
+++ Pattern4.java
@@ -0,0 +1,21 @@
+
+public class Pattern4 {
+	public static void printTriangle(int n) {
+		for (int i = 0; i < n; i++) {
+			for (int j = n - i; j > 1; j--) {
+				System.out.print(" ");
+			}
+
+			for (int j = 0; j <= i; j++) {
+				System.out.print("* ");
+			}
+			System.out.println();
+		}
+	}
+
+	public static void main(String[] args) {
+		int n = 4;
+		printTriangle(n);
+	}
+
+}
